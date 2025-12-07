f5f25f4f9b7e982a42b7a14a360c5442311bf674
viqueen <hasnae@viqueen.org>
2025-12-07 20:17:40 +1100

    happy lint

--- platform/backend/internal/api/auth/v1/workos/workos.go
+++ platform/backend/internal/api/auth/v1/workos/workos.go
@@ -1,12 +1,7 @@
 package api_auth_v1_workos
 
 import (
-	"github.com/gin-gonic/gin"
 	authv1 "platform/backend/pkg/auth/v1"
-)
-
-const (
-	SessionKeyAuthState = "workos_auth_state"
 )
 
 type workosHandler struct {
