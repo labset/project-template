aba7ce90b0585c02cca34473a942ce59f48b011f
viqueen <hasnae@viqueen.org>
2025-12-07 20:30:36 +1100

    happy lint

--- platform/backend/internal/api/auth/v1/workos/workos_logout.go
+++ platform/backend/internal/api/auth/v1/workos/workos_logout.go
@@ -3,6 +3,6 @@ package api_auth_v1_workos
 import "github.com/gin-gonic/gin"
 
 func (w *workosHandler) Logout(ctx *gin.Context) {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
